586866148420a99c11ef97c44a466965b8161192
Raghu Vamshi <raghuvamshi@yellowmessenger.com>
2021-06-08 14:05:01 +0530

    SECOND

--- Others/new.js
+++
@@ -1,7 +0,0 @@
-const m = require('moment-timezone');
-let d = '2021-06-15T01:00:00+10:00';
-let t = m('2021-06-15T01:00:00+10:00');
-let n = m(t).tz('Australia/Sydney');
-console.log(t);
-console.log(d);
-console.log(n);
